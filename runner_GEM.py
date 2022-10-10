fdbf8764bea743b72a62c9c738b5cf54240ec780
Steffengra <steffengrac@gmail.com>
2022-10-10 11:14:12 +0200

    implemented network test method

--- runner_GEM.py
+++ runner_GEM.py
@@ -221,6 +221,7 @@ class Runner:
             # reset simulation for next episode
             sim.reset()
 
+        # POST LOOP-----------------------------------------------------------------------------------------------------
         # save trained networks locally
         save_networks()
 
@@ -239,6 +240,138 @@ class Runner:
             if self.config.verbosity == 1:
                 self.profiler.print_stats(sort='cumulative')
             self.profiler.dump_stats(Path(self.config.performance_profile_path, f'{training_name}.profile'))
+
+        # plots
+        plot_scatter_plot(per_episode_metrics['reward_per_step'],
+                          title='Per Step Reward')
+        plot_scatter_plot(per_episode_metrics['priority_timeouts_per_occurrence'],
+                          title='Per Occurrence Priority Timeouts')
+
+    def test(
+            self,
+            probability_critical_events: float,
+            policy_network_path: None or str = None,
+            name: str = '',
+    ) -> None:
+
+        def progress_print() -> None:
+            progress = (episode_id * self.config.num_steps_per_episode + step_id + 1) / self.config.steps_total
+            timedelta = datetime.now() - real_time_start
+            finish_time = real_time_start + timedelta / progress
+
+            print(f'\rSimulation completed: {progress:.2%}, '
+                  f'est. finish {finish_time.hour:02d}:{finish_time.minute:02d}:{finish_time.second:02d}', end='')
+
+        def allocate(st):
+            return allocator_network.call(st[newaxis]).numpy().squeeze()
+
+
+        # GENERAL SETUP-------------------------------------------------------------------------------------------------
+        testing_name = f'testing_{name}'
+        if self.config.verbosity == 1:
+            print('\n' + testing_name)
+
+        # log start time
+        real_time_start = datetime.now()
+
+        # enable compute profiling
+        if self.config.toggle_profiling:
+            self.profiler.enable()
+
+        # set up testing environment
+        sim = Simulation(
+            config=self.config,
+            rng=self.rng,
+        )
+        if self.config.verbosity == 1:
+            print(f'Expected load: {sim.get_expected_load():.2f}')
+
+        # set up allocator
+        allocator_network = load_model(policy_network_path)
+
+        # set up global metrics
+        per_episode_metrics: dict = {
+            'reward_per_step': -infty * ones(self.config.num_episodes),
+            'priority_timeouts_per_occurrence': +infty * ones(self.config.num_episodes),
+        }
+
+        # MAIN LOOP-----------------------------------------------------------------------------------------------------
+        for episode_id in range(self.config.num_episodes):
+
+            # set up episode metrics
+            episode_metrics: dict = {
+                'rewards': -infty * ones(self.config.num_steps_per_episode),
+                'priority_timeouts': +infty * ones(self.config.num_steps_per_episode),
+            }
+
+            # determine first state
+            state_next = sim.gather_state()
+
+            # episode step loop
+            for step_id in range(self.config.num_steps_per_episode):
+                # simulation_step = episode_id * self.config.num_steps_per_episode + step_id
+
+                # determine state
+                state_current = state_next
+
+                # find allocation action based on state
+                bandwidth_allocation_solution = allocate(state_current)
+                noisy_bandwidth_allocation_solution = bandwidth_allocation_solution
+
+                # step simulation based on action
+                (
+                    step_reward,
+                    unweighted_step_reward_components,
+                ) = sim.step(
+                    percentage_allocation_solution=noisy_bandwidth_allocation_solution,
+                    critical_events_chance=probability_critical_events,
+                )
+
+                # determine new state
+                state_next = sim.gather_state()
+
+                # log step results
+                episode_metrics['rewards'][step_id] = step_reward
+                episode_metrics['priority_timeouts'][step_id] = unweighted_step_reward_components['sum_priority_timeouts']
+
+                # progress print
+                if self.config.verbosity == 1:
+                    if step_id % 50 == 0:
+                        progress_print()
+
+            # log episode results
+            per_episode_metrics['reward_per_step'][episode_id] = (
+                    sum(episode_metrics['rewards']) / self.config.num_steps_per_episode
+            )
+            per_episode_metrics['priority_timeouts_per_occurrence'][episode_id] = (
+                    sum(episode_metrics['priority_timeouts']) / self.config.num_steps_per_episode / (
+                        probability_critical_events + self.config.tiny_numerical_value)
+            )
+
+            # print episode results
+            if self.config.verbosity == 1:
+                per_step_reward = sum(episode_metrics['rewards']) / self.config.num_steps_per_episode
+                relative_timeouts = (sum(episode_metrics['priority_timeouts'])
+                                     / self.config.num_steps_per_episode
+                                     / (probability_critical_events + self.config.tiny_numerical_value))
+                print('\n', end='')
+                print(f'episode per step reward: {per_step_reward:.2f}')
+                print(f'episode per occurrence priority timeouts: {relative_timeouts:.2f}')
+
+            # reset simulation for next episode
+            sim.reset()
+
+        # TEARDOWN------------------------------------------------------------------------------------------------------
+        # save logged results
+        with gzip_open(Path(self.config.log_path, f'{testing_name}_per_episode_metrics.gzip'), 'wb') as file:
+            pickle_dump(per_episode_metrics, file=file)
+
+        # end compute performance profiling
+        if self.config.toggle_profiling:
+            self.profiler.disable()
+            if self.config.verbosity == 1:
+                self.profiler.print_stats(sort='cumulative')
+            self.profiler.dump_stats('train_critical_events.profile')
 
         # plots
         plot_scatter_plot(per_episode_metrics['reward_per_step'],
