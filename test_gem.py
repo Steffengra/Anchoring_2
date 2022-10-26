940465bc8d5df9433c2c6012a8aa7fd4aefb5c02
Steffengra <steffengrac@gmail.com>
2022-10-26 17:04:55 +0200

    finalized test script

--- test_gem.py
+++ test_gem.py
@@ -16,18 +16,23 @@ from runner_GEM import Runner
 def main():
     def train():
         pass
+
         # train_on_hundred_percent_crit_events()
-        train_on_normal_crit_events_gem_hundred_percent_crit_events_pretrained()
+        # train_on_normal_crit_events_gem_hundred_percent_crit_events_pretrained()
         train_on_no_crit_events_gem_hundred_percent_crit_events_pretrained_twice()
+
+        # train_on_normal_crit_events()
 
     def test():
         pass
         runner.config.update_num_episodes(new_num_episodes=5)
         runner.config.update_num_steps_per_episode(new_steps_per_episode=200_000)
 
-        test_hundred_percent_crit_events_scheduler_on_normal()
-        test_normal_gem_hundred_pretrained_on_normal()
-        test_no_gem_hundred_pretrained_twice_on_normal()
+        # test_hundred_percent_crit_events_scheduler_on_normal()
+        # test_normal_gem_hundred_pretrained_on_normal()
+        test_zero_gem_hundred_pretrained_twice_on_normal()
+
+        # test_normal_pretrained_on_normal()
 
     # TRAINING WRAPPERS-------------------------------------------------------------------------------------------------
     def _train_network(
@@ -35,11 +40,10 @@ def main():
             prior_task_sample_names: None or str = None,
             preload_value_parameters: None or str = None,
             preload_policy_parameters: None or str = None,
+            name_extra: None or str = None,
     ):
-        if prior_task_sample_names is None:
-            training_name = f'training_{probability_crit_events}_base'
-        else:
-            training_name = f'training_{probability_crit_events}_gem'
+
+        training_name = f'training_{probability_crit_events}_{name_extra}'
 
         value_network_path = None
         if preload_value_parameters:
@@ -49,7 +53,7 @@ def main():
             policy_network_path = Path(config.model_path, f'actor_allocation_training_{preload_value_parameters}')
 
         prior_task_samples_path = [
-            Path(config.model_path, f'experiences_training_{prior_task_sample_name}_samples.gzip')
+            Path(config.model_path, f'experiences_training_{prior_task_sample_name}.gzip')
             for prior_task_sample_name in prior_task_sample_names
         ]
 
@@ -64,35 +68,53 @@ def main():
     def train_on_hundred_percent_crit_events():
         _train_network(
             probability_crit_events=1.0,
+            prior_task_sample_names=[],
+            name_extra='critical',
         )
 
     def train_on_normal_crit_events_gem_hundred_percent_crit_events_pretrained():
         _train_network(
             probability_crit_events=config.normal_priority_job_probability,
-            prior_task_sample_names=['1.0_base_2048'],
-            preload_value_parameters='1.0_base',
-            preload_policy_parameters='1.0_base',
+            prior_task_sample_names=['1.0_critical'],
+            preload_value_parameters='1.0_critical',
+            preload_policy_parameters='1.0_critical',
+            name_extra='gem'
         )
 
     def train_on_no_crit_events_gem_hundred_percent_crit_events_pretrained_twice():
         _train_network(
+            probability_crit_events=0.0,
+            prior_task_sample_names=['1.0_critical'],
+            preload_policy_parameters='0.0001_gem',
+            name_extra='twice',
+        )
+
+    def train_on_normal_crit_events():
+        _train_network(
             probability_crit_events=config.normal_priority_job_probability,
-            prior_task_sample_names=['1.0_base_2048'],
-            preload_policy_parameters=''
+            prior_task_sample_names=[],
+            name_extra='base',
         )
 
     # TESTING WRAPPERS--------------------------------------------------------------------------------------------------
-    def _test_network():
-        pass
+    def _test_network(network_name: str):
+        runner.test(
+            probability_critical_events=config.normal_priority_job_probability,
+            policy_network_path=Path(config.model_path, f'actor_allocation_training_{network_name}'),
+            name=network_name,
+        )
 
     def test_hundred_percent_crit_events_scheduler_on_normal():
-        pass
+        _test_network('1.0_critical')
 
     def test_normal_gem_hundred_pretrained_on_normal():
-        pass
+        _test_network('0.0001_gem')
 
-    def test_no_gem_hundred_pretrained_twice_on_normal():
-        pass
+    def test_zero_gem_hundred_pretrained_twice_on_normal():
+        _test_network('0.0_twice')
+
+    def test_normal_pretrained_on_normal():
+        _test_network('0.0001_base')
 
     # BODY--------------------------------------------------------------------------------------------------------------
     config = Config()
